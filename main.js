9af7eff3607b0095df18a3ff14392022fcb54759
Artur01300 <artur.karapetyan@outlook.fr>
2022-03-15 14:41:14 +0100

    Type Rest

--- main.js
+++ main.js
@@ -1,70 +1,89 @@
 /*
-  Function Type
-  - Paramètres facutatifs
-  - Paramètres par défaut
+  Type Rest
+  - Rest dans les paramètres de fonction - Type
+  - Rest dans le Type Function
+  
+  Les parapètres "obligatoires, facutlatifs et par défaut" ont tous une chose en commun:
+  ils font référance à un paramètre à la fois.
+  Parfois, vous shohaitez travailler avec plusieurs paramètres (Paramètre groupés), ou bien vous ne savez peut-être pas
+  combien de paramètres un fonction va exiger.
+  En JS, vous vouvez travailler avec les argumants directement en utilisant le variable "arguments" qui est accessible dans
+  le cors de la fonction
 */
 //1. exerciece
-// function buildName(firstName: string, lastName: string){
-//   return firstName + " " + lastName
+// ici on passe < arguments > car on connait pas en avance ces arguments.
+// let colors = function(){
+//   console.log(arguments)
 // }
-var result1 = buildName('Bobe'); //Bob undefinded
-// console.log(result1);
-//ici TS n'est pas content par contr pour JS pas de problème (il ignore le <Hello>)
-// let result2 = buildName('Bobe', 'Adams', 'Hello');// Bobe Adams
-// console.log(result2)
-// ici tout est correct
-// let result3 = buildName('Bobe', 'Adams');// Bobe Adam
-// console.log(result3)
-//------------------------------------------- 
+// colors('green', 'red', 'oranage') // js ok  - TS non
 //2. exerciece
-//< ? > signifie c'est pas emportant le 2em argument
-function buildName(firstName, lastName) {
-    return firstName + " " + lastName;
+// Pour éviter les trets rouges on passe les arguement
+// let colors = function(arg1: string, arg2: string, arg3: string){
+//   console.log('Arguments *******', arguments);
+//   console.log(typeof arguments); //object
+//   console.log(arguments.length) //3
+// }
+// colors('green', 'red', 'oranage') // js ok  - TS ok
+//3. exerciece
+// avec cest exerciece on affiche toute les valeurs dans le DOM
+// comment le faire si on veus separer les arguemes pour afficher autremant, par exemple: arg1 séparer de les autres argumant ?
+// Pour cet reson on utilis < Rest >
+// const divColors = document.getElementById('colors');
+// let colors = function(arg1: string, arg2: string, arg3: string, arg4: string){
+//   const h1 = document.createElement('H1');
+//   h1.innerHTML = `Titre: ${arg1}`;
+//   divColors.appendChild(h1);
+//   const ul = document.createElement('ul');
+//   for (let i in arguments) {
+//     const elements = arguments[i];
+//     let li = document.createElement('li');
+//     li.innerHTML = arguments[i];
+//     ul.appendChild(li);
+//   }
+//   divColors.appendChild(ul);
+// }
+// colors('list 1:', 'green', 'red', 'oranage');
+// colors('list 2:', 'Bleu', 'Black', 'Pink');
+//3. exerciece 
+// Rest dans les paramètres de fonction - Type
+var divColors = document.getElementById('colors');
+// < ...restOfColors > est le rest. en résumé le Rest dissocie le rest (dissocie les arguments)
+var colors = function (arg1) {
+    var restOfColors = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        restOfColors[_i - 1] = arguments[_i];
+    }
+    var h1 = document.createElement('H1');
+    h1.innerHTML = "Titre: ".concat(arg1);
+    divColors.appendChild(h1);
+    var ul = document.createElement('ul');
+    for (var i in restOfColors) {
+        var elements = restOfColors[i];
+        var li = document.createElement('li');
+        li.innerHTML = restOfColors[i];
+        ul.appendChild(li);
+    }
+    divColors.appendChild(ul);
+};
+colors('list 1:', 'green', 'red', 'oranage');
+colors('list 2:', 'Bleu', 'Black', 'Pink');
+//3. exerciece 
+// Rest dans le Type Function
+function buildName(firstName) {
+    var restOfName = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        restOfName[_i - 1] = arguments[_i];
+    }
+    return firstName + " " + restOfName.join(" ");
 }
-var result4 = buildName('Bobe'); //Bob undefinded
-// console.log(result4);
-// vour éviter < undefinded > dans le console log, on peut vérifier avec if else:
-function buildName2(firstName, lastName) {
-    if (lastName)
-        return firstName + " " + lastName;
-    else
-        return firstName;
+// let buildNameFun = buildName;// on peut lesser comme ça ou 
+var buildNameFun = buildName;
+//ou 
+function sum(sum1) {
+    var sum2 = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        sum2[_i - 1] = arguments[_i];
+    }
+    return sum1 + sum2;
 }
-var result5 = buildName2('Bobe'); //Bob sans undefinded
-// console.log(result5);
-//2. 2em argument sera remplaser automatiquent si il n'est pas fournie
-function buildName3(firstName, lastName) {
-    if (lastName === void 0) { lastName = "Smith"; }
-    return firstName + " " + lastName;
-}
-var result6 = buildName3('Bobe'); //Bob Smith
-// console.log(result6);
-//ici le 2em valeur est remlacé (écrasé)  < Smith >
-var result7 = buildName3('Bobe', 'Artur'); //Bob Artur
-// console.log(result7);
-//on peut également dire que 2em valeur est undefined, puis il sera remplacer par <Smith>
-var result8 = buildName3('Bobe', undefined); //Bob Smith
-console.log(result8);
-/*
-  Les paramètres initialisés par défaut qui suivent tous les paramètes requis sant trétés comme facutatifs,
-  et comme les paramètes facultatifs, ils peuvent être omis lors de l'appel de leur fonction respective.
-  Les paramètre facultatifs on peut pas placer en premier position
-
-  paramètes requis: < firstName: string >
-  paramètres initialisés par défaut: < lastName?: string > sont des valeur faculatifs
-
-*/
-function buildName4(firstName, lastName) {
-}
-/*
-  Contrairement aux paramètres facultatifs simples, les paramètres initialisés par défaut
-  n'ont pas besoin de se positionner obligatoirement après paramètres requis. Si un paramètre initialisé par défaut précède un
-  paramètre obligatoire, les utilisateurs doivent passer explicitement "undefinded" pour pouvoir obtenir la valeur initialisée par défaut.
-
-  aramètres facultatifs simples: < lastName?: string >
-  paramètres initialisés par défaut: < firstName: "Will" >
- 
-*/
-function buildName5(firstName, lastName) {
-    return firstName + " " + lastName;
-}
+buildNameFun = sum;
--- main.ts
+++ main.ts
@@ -1,83 +1,101 @@
 /*
-  Function Type
-  - Paramètres facutatifs
-  - Paramètres par défaut
+  Type Rest
+  - Rest dans les paramètres de fonction - Type
+  - Rest dans le Type Function
+  
+  Les parapètres "obligatoires, facutlatifs et par défaut" ont tous une chose en commun:
+  ils font référance à un paramètre à la fois.
+  Parfois, vous shohaitez travailler avec plusieurs paramètres (Paramètre groupés), ou bien vous ne savez peut-être pas
+  combien de paramètres un fonction va exiger.
+  En JS, vous vouvez travailler avec les argumants directement en utilisant le variable "arguments" qui est accessible dans
+  le cors de la fonction
 */
+
 
 //1. exerciece
-
-// function buildName(firstName: string, lastName: string){
-//   return firstName + " " + lastName
+// ici on passe < arguments > car on connait pas en avance ces arguments.
+// let colors = function(){
+//   console.log(arguments)
 // }
 
-let result1 = buildName('Bobe'); //Bob undefinded
-// console.log(result1);
+// colors('green', 'red', 'oranage') // js ok  - TS non
 
-//ici TS n'est pas content par contr pour JS pas de problème (il ignore le <Hello>)
-// let result2 = buildName('Bobe', 'Adams', 'Hello');// Bobe Adams
-// console.log(result2)
-
-// ici tout est correct
-// let result3 = buildName('Bobe', 'Adams');// Bobe Adam
-// console.log(result3)
-
-//------------------------------------------- 
 
 //2. exerciece
-//< ? > signifie c'est pas emportant le 2em argument
-function buildName(firstName: string, lastName?: string){
-  return firstName + " " + lastName
-}
+// Pour éviter les trets rouges on passe les arguement
+// let colors = function(arg1: string, arg2: string, arg3: string){
+//   console.log('Arguments *******', arguments);
+//   console.log(typeof arguments); //object
+//   console.log(arguments.length) //3
 
-let result4 = buildName('Bobe'); //Bob undefinded
-// console.log(result4);
+// }
 
-// vour éviter < undefinded > dans le console log, on peut vérifier avec if else:
-function buildName2(firstName: string, lastName?: string){
-  if (lastName) return firstName + " " + lastName;
-  else return firstName 
-}
-let result5 = buildName2('Bobe'); //Bob sans undefinded
-// console.log(result5);
+// colors('green', 'red', 'oranage') // js ok  - TS ok
 
-//2. 2em argument sera remplaser automatiquent si il n'est pas fournie
-function buildName3(firstName: string, lastName = "Smith"){
-  return firstName + " " + lastName
-}
+//3. exerciece
+// avec cest exerciece on affiche toute les valeurs dans le DOM
+// comment le faire si on veus separer les arguemes pour afficher autremant, par exemple: arg1 séparer de les autres argumant ?
+// Pour cet reson on utilis < Rest >
+// const divColors = document.getElementById('colors');
 
-let result6 = buildName3('Bobe'); //Bob Smith
-// console.log(result6);
-
-//ici le 2em valeur est remlacé (écrasé)  < Smith >
-let result7 = buildName3('Bobe', 'Artur'); //Bob Artur
-// console.log(result7);
-
-//on peut également dire que 2em valeur est undefined, puis il sera remplacer par <Smith>
-let result8 = buildName3('Bobe', undefined); //Bob Smith
-console.log(result8);
-
-/*
-  Les paramètres initialisés par défaut qui suivent tous les paramètes requis sant trétés comme facutatifs,
-  et comme les paramètes facultatifs, ils peuvent être omis lors de l'appel de leur fonction respective.
-  Les paramètre facultatifs on peut pas placer en premier position
-
-  paramètes requis: < firstName: string >
-  paramètres initialisés par défaut: < lastName?: string > sont des valeur faculatifs
-
-*/
-function buildName4(firstName: string, lastName?: string){
+// let colors = function(arg1: string, arg2: string, arg3: string, arg4: string){
   
+//   const h1 = document.createElement('H1');
+//   h1.innerHTML = `Titre: ${arg1}`;
+//   divColors.appendChild(h1);
+
+//   const ul = document.createElement('ul');
+
+//   for (let i in arguments) {
+//     const elements = arguments[i];
+//     let li = document.createElement('li');
+//     li.innerHTML = arguments[i];
+//     ul.appendChild(li);
+//   }
+//   divColors.appendChild(ul);
+// }
+// colors('list 1:', 'green', 'red', 'oranage');
+// colors('list 2:', 'Bleu', 'Black', 'Pink');
+
+
+//3. exerciece 
+// Rest dans les paramètres de fonction - Type
+const divColors = document.getElementById('colors');
+
+// < ...restOfColors > est le rest. en résumé le Rest dissocie le rest (dissocie les arguments)
+let colors = function(arg1: string, ...restOfColors:string[]){
+  
+  const h1 = document.createElement('H1');
+  h1.innerHTML = `Titre: ${arg1}`;
+  divColors.appendChild(h1);
+
+  const ul = document.createElement('ul');
+
+  for (let i in restOfColors) {
+    const elements = restOfColors[i];
+    let li = document.createElement('li');
+    li.innerHTML = restOfColors[i];
+    ul.appendChild(li);
+  }
+  divColors.appendChild(ul);
+}
+colors('list 1:', 'green', 'red', 'oranage');
+colors('list 2:', 'Bleu', 'Black', 'Pink');
+
+
+//3. exerciece 
+// Rest dans le Type Function
+
+function buildName(firstName: string, ...restOfName:string[]){
+  return firstName + " " + restOfName.join(" ");
 }
 
-/*
-  Contrairement aux paramètres facultatifs simples, les paramètres initialisés par défaut
-  n'ont pas besoin de se positionner obligatoirement après paramètres requis. Si un paramètre initialisé par défaut précède un 
-  paramètre obligatoire, les utilisateurs doivent passer explicitement "undefinded" pour pouvoir obtenir la valeur initialisée par défaut.
+// let buildNameFun = buildName;// on peut lesser comme ça ou 
+let buildNameFun:(fnam: string, ...rest: string[]) => string = buildName;
 
-  aramètres facultatifs simples: < lastName?: string >
-  paramètres initialisés par défaut: < firstName: "Will" > 
- 
-*/
-function buildName5(firstName: "Will", lastName: string){
-  return firstName + " " + lastName
+//ou 
+function sum(sum1: string, ...sum2: string[]){
+  return sum1 + sum2
 }
+
+buildNameFun = sum;
