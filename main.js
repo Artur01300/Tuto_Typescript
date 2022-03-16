5343d4e17639c10d17d8e39848290a3a81c8baf2
Artur01300 <artur.karapetyan@outlook.fr>
2022-03-16 10:10:34 +0100

    25. Unions Types

--- main.js
+++ main.js
@@ -1,32 +1,70 @@
-//CallBack Function Type (Fonction de rappel)
-// 1. exercice
-// fonction normal 
-function gift(age) {
-    return age + 3;
+//Union Type      string | number | boolean
+// 1. Union types
+// exemple: ici on peut assignier à firstNage une autre valeur comme, par exemple number 
+// let firstName = "Roger";
+// firstName = 200;
+//Mais, c'est posible avec Union types: let firstName: string | number:
+var firstName = "Roger";
+firstName = 200;
+// on peut ajouter si on veut booleant, ces aussi possible
+// comment gèrer ses parapètre en tent que fonction ?
+function sum(arg1, arg2) {
+    return arg1 + arg2;
 }
-gift(40);
-// 2. exercice CallBack Function
-//  - CallBack Function : c'est à dire dans le gift2(40) on passe un autre fuction << gift2(40, () =>) >>
-//  - il faut également définire 2em paramète pour le callbak function
-// function gift2(age: number, sum:(arg: number) => number){
-//   const memberAge = age + 3;
-//   sum(memberAge)//j'envoque à la fuction sum et je passe la valeur memberAge
-// }
-// gift2(40, num => { // CollBack function < num => {} 
-//   //une fois ma function envoquer dans la ligne 18 j'éxécute ma collBack function
-//   console.log(num)//43
-// });
+console.log(sum(20, 40)); //60
+console.log(sum("Hello", 40)); // TS Error - JS OK
 /*
-  function gift2(age: number, sum:(arg: number) => ){
-    pour résudre le problème dans la paramètre < sum >:
-    Mon < sum:(arg: number) => number)> ne function pas au niveau de callBack fuction pour ce la ajouter voide dans la paramètre
-    Attation ! à la place void sans savoir on peut ajouter Any (ces pas un bon practique)
+  comment faire dans cet cas pour notre fonction qu'elle accepte au niveau de 1er argument un type number et un type string ?
+  Pour cela on va utiliser les Unions types
 */
-// Résultat Final
-function gift3(age, sum) {
-    var memberAge = age + 3;
-    sum(memberAge);
+// 1. exemple pour union types
+function concatenate(arg1, arg2) {
+    /*
+     ici on a une alert: < Impossible d'appliquer l'opérateur '+' aux types 'string | number' et 'string | number'. >
+      le TS il n'arrive pas les distanger ou gèrer les arguments car il sait pas si c'est un string ou number.
+      Pour résoudre cet proplème on ajoute if else ou farie ça < return arg1 + " " + arg2
+    */
+    return arg1 + arg2;
 }
-gift3(40, function (num) {
-    console.log(num); //43
-});
+console.log(concatenate(10, 20));
+console.log(concatenate("Hello", 20));
+//2 solutions
+function concatenate2(arg1, arg2) {
+    var result;
+    if (typeof arg1 === "number" && typeof arg2 === "number") {
+        result = arg1 + arg2; //number
+    }
+    else if (typeof arg1 === "string" && typeof arg2 === "string") {
+        result = arg1 + arg2; // string
+    }
+    else {
+        /*
+          isi on a une error: Impossible d'appliquer l'opérateur '+' aux types 'string | number' et 'string | number'
+          parce que on ne peut pas additionner 2 types differants.
+          Solution: il faut transformer les valeur en strige avec la methode < tostring() >
+        */
+        result = arg1 + arg2;
+    }
+    return result;
+}
+// console.log(concatenate(10, 20));
+// console.log(concatenate("Hello", 20))
+//3 solutions final
+function concatenate3(arg1, arg2) {
+    var result;
+    if (typeof arg1 === "number" && typeof arg2 === "number") {
+        result = arg1 + arg2; //number
+    }
+    else if (typeof arg1 === "string" && typeof arg2 === "string") {
+        result = arg1 + arg2; // string
+    }
+    else {
+        result = arg1.toString() + arg2.toString();
+        //ou:  result = arg1 + " " + arg2;
+    }
+    return result;
+}
+console.log(concatenate3(10, 20)); //30
+console.log(concatenate3("Hello", "World")); // HelloWorld
+console.log(concatenate3("Hello", 20)); //Hello20
+console.log(concatenate3(20, "Hello")); //20Hello
