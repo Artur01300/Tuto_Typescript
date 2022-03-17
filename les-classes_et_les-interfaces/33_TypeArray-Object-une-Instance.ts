b0ae7acc1aaeb5144f04370a68e2bf6d4b9346ac
Artur01300 <artur.karapetyan@outlook.fr>
2022-03-17 19:47:35 +0100

    33. Type Array Object d'une Instance

--- les-classes_et_les-interfaces/33_TypeArray-Object-une-Instance.ts
+++ les-classes_et_les-interfaces/33_TypeArray-Object-une-Instance.ts
@@ -1,36 +1,49 @@
+
 /*
     Type Array Object d'une Instance
 
-        - < Type Array Object d'une Instance > consicte à placer des objets dans un array, sauf que ça va pas être n'aport quel objet:
+        - < Type Array Object d'une Instance > consicte à placer des objets dans un array, sauf que ça va pas être n'aport quel objet: 
         ça va être des objets ayons des carachteristique d'un objet ayon comme tipe < Invoice2 >, c'est à dir, un objet généré via une class
         bein spécifique.
 */
-var Invoice2 = /** @class */ (function () {
-    function Invoice2(client, product, price) {
+
+class Invoice2 {
+    client: string;
+    product: string;
+    price: number;
+
+    constructor(client: string, product: string, price: number){
         this.client = client;
         this.product = product;
         this.price = price;
     }
-    return Invoice2;
-}());
-var instInvoice1 = new Invoice2('Dora', 'Template', 45);
-var instInvoice2 = new Invoice2('Artur', 'filem', 80);
+}
+
+const instInvoice1 = new Invoice2('Dora', 'Template', 45);
+const instInvoice2 = new Invoice2('Artur', 'filem', 80);
+
 /*
     sénario:
     Nous avon créer un arrey pour placer nos objet à l'entèrieur, mais ça vas pas être n'aport le quel objet, ça va être des objets
     qui obeissent à la contrainte de type < Invoice2 >
 */
+
 // 1 Exemple:
+
 // avec cet exemple on peut metre n'aport quel type à l'enterieur de cet Arrey, mais c'est pas ça qu'on veut fair
-var invoicArray = [];
+let invoicArray = [];
+
 // dans notre cas on veut créer un arrey qui va contenire simplement des objets ayont les caractairistique de la tiyp < Invoice2 >
-var invoicArray2 = [];
+let invoicArray2:Invoice2[] = [];
+
 //Suit à ça je peut utiliser la méthode < push > et à prèsant je peut ajecter des objets dans cet arrey là
-invoicArray2.push;
+invoicArray2.push
+
 // Solution Final : 
-invoicArray2.push(instInvoice1, instInvoice2);
+invoicArray2.push(instInvoice1, instInvoice2); 
 //ou
-invoicArray2.push(instInvoice1, { client: "Armen", product: "TV", price: 99 });
+invoicArray2.push(instInvoice1, {client: "Armen", product: "TV", price: 99});
 //ou
-invoicArray2.push({ client: "Armen", product: "TV", price: 99 });
-console.log('invoicArray2', invoicArray2);
+invoicArray2.push({client: "Armen", product: "TV", price: 99});
+
+console.log('invoicArray2', invoicArray2)
